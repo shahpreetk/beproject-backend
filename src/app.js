e65cd39b28fead63098dde8fca46e57a370a0bb8
Preet Shah <shahpreetk@gmail.com>
2021-03-17 17:41:18 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const bookingsRouter = require("./routers/booking");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(bookingsRouter);
